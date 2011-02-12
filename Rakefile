74c0fed1d8cb672b008270e8f6fe423175e71524
Stefan Daschek <stefan@die-antwort.eu>
2011-02-12 01:54:08 +0100

    Update da-suspenders.rb and template files according to our needs.

--- Rakefile
+++ Rakefile
@@ -1,10 +1,5 @@
-require 'rubygems'
-require 'rake'
-require 'cucumber/rake/task'
-require 'date'
-
-TEST_PROJECT = 'test_project'
-SUSPENDERS_GEM_VERSION = '0.1.0.beta.3'
+require "bundler"
+require "cucumber/rake/task"
 
 #############################################################################
 #
@@ -13,6 +8,8 @@ SUSPENDERS_GEM_VERSION = '0.1.0.beta.3'
 #############################################################################
 
 Cucumber::Rake::Task.new
+
+TEST_PROJECT = "test_project"
 
 namespace :test do
   desc "A full suspenders app's test suite"
@@ -23,7 +20,7 @@ namespace :test_project do
   desc 'Suspend a new project. Pass REPO=... to change the Suspenders repo.'
   task :generate do
     FileUtils.rm_rf(TEST_PROJECT)
-    sh './bin/suspenders', 'create', TEST_PROJECT, ENV['REPO'].to_s
+    sh './bin/da-suspenders', 'create', TEST_PROJECT, ENV['REPO'].to_s
   end
 
   desc 'Remove a suspended project'
@@ -38,83 +35,3 @@ end
 
 desc 'Run the test suite'
 task :default => ['test:full']
-
-#############################################################################
-#
-# Helper functions
-#
-#############################################################################
-
-def name
-  @name ||= Dir['*.gemspec'].first.split('.').first
-end
-
-def version
-  SUSPENDERS_GEM_VERSION
-end
-
-def date
-  Date.today.to_s
-end
-
-def gemspec_file
-  "#{name}.gemspec"
-end
-
-def gem_file
-  "#{name}-#{version}.gem"
-end
-
-def replace_header(head, header_name)
-  head.sub!(/(\.#{header_name}\s*= ').*'/) { "#{$1}#{send(header_name)}'"}
-end
-
-#############################################################################
-#
-# Packaging tasks
-#
-#############################################################################
-
-task :release => :build do
-  unless `git branch` =~ /^\* master$/
-    puts "You must be on the master branch to release!"
-    exit!
-  end
-  sh "git commit --allow-empty -a -m 'Release #{version}'"
-  sh "git tag v#{version}"
-  sh "git push origin master"
-  sh "git push --tags"
-  sh "gem push pkg/#{name}-#{version}.gem"
-end
-
-task :build => :gemspec do
-  sh "mkdir -p pkg"
-  sh "gem build #{gemspec_file}"
-  sh "mv #{gem_file} pkg"
-end
-
-task :gemspec do
-  # read spec file and split out manifest section
-  spec = File.read(gemspec_file)
-  head, manifest, tail = spec.split("  # = MANIFEST =\n")
-
-  # replace name version and date
-  replace_header(head, :name)
-  replace_header(head, :version)
-  replace_header(head, :date)
-
-  # determine file list from git ls-files
-  files = `git ls-files`.
-    split("\n").
-    sort.
-    reject { |file| file =~ /^\./ }.
-    reject { |file| file =~ /^(rdoc|pkg)/ }.
-    map { |file| "    #{file}" }.
-    join("\n")
-
-  # piece file back together and write
-  manifest = "  s.files = %w[\n#{files}\n  ]\n"
-  spec = [head, manifest, tail].join("  # = MANIFEST =\n")
-  File.open(gemspec_file, 'w') { |io| io.write(spec) }
-  puts "Updated #{gemspec_file}"
-end
--- template/da-suspenders.rb
+++ template/da-suspenders.rb
@@ -1,24 +1,11 @@
-require 'net/http'
-require 'net/https'
+require "fileutils"
+require "pathname"
+require "trout"
 
 template_root = File.expand_path(File.join(File.dirname(__FILE__)))
 source_paths << File.join(template_root, "files")
 
 # Helpers
-
-def concat_file(source, destination)
-  contents = IO.read(find_in_source_paths(source))
-  append_file destination, contents
-end
-
-def replace_in_file(relative_path, find, replace)
-  path = File.join(destination_root, relative_path)
-  contents = IO.read(path)
-  unless contents.gsub!(find, replace)
-    raise "#{find.inspect} not found in #{relative_path}"
-  end
-  File.open(path, "w") { |file| file.write(contents) }
-end
 
 def action_mailer_host(rails_env, host)
   inject_into_file(
@@ -28,126 +15,148 @@ def action_mailer_host(rails_env, host)
   )
 end
 
-def download_file(uri_string, destination)
-  uri = URI.parse(uri_string)
-  http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = true if uri_string =~ /^https/
-  request = Net::HTTP::Get.new(uri.path)
-  contents = http.request(request).body
-  path = File.join(destination_root, destination)
-  File.open(path, "w") { |file| file.write(contents) }
-end
-
 def origin
-  "git://github.com/thoughtbot/suspenders.git"
+  "git://github.com/die-antwort/da-suspenders.git"
 end
 
 def trout(destination_path)
+  parent = Pathname.new(destination_path).parent
+  FileUtils.mkdir_p(parent) unless File.exists?(parent) 
   run "trout checkout --source-root=template/trout #{destination_path} #{origin}"
 end
 
-say "Getting rid of files we don't use"
+# Actions
 
-remove_file "README"
-remove_file "public/index.html"
-remove_file "public/images/rails.png"
-
-say "Setting up the staging environment"
-
-run "cp config/environments/production.rb config/environments/staging.rb"
-
-say "Creating suspenders views"
-
-empty_directory "app/views/shared"
-copy_file "_flashes.html.erb", "app/views/shared/_flashes.html.erb"
-copy_file "_javascript.html.erb", "app/views/shared/_javascript.html.erb"
-template "suspenders_layout.html.erb.erb",
-         "app/views/layouts/application.html.erb",
-         :force => true
-
-say "Let's use jQuery"
-
-%w(jquery jquery-ui).each do |file|
-  trout "public/javascripts/#{file}.js"
+def create_gemfile_and_install_gems
+  say "Creating Gemfile and installing gems (this may take a while)", :yellow
+  trout "Gemfile"
+  run "bundle install --local"
+end
+  
+def add_staging_environment
+  say "Adding staging environment", :yellow
+  run "cp config/environments/production.rb config/environments/staging.rb"
 end
 
-download_file "https://github.com/rails/jquery-ujs/raw/master/src/rails.js",
-          "public/javascripts/rails.js"
+def setup_action_mailer
+  say "Setting up action mailer config", :yellow
+  action_mailer_host "development", "localhost:3000"
+  action_mailer_host "test",        "example.com"
+  action_mailer_host "staging",     "#{app_name}.dev.die-antwort.eu"
+  action_mailer_host "production",  "FIXME"
+end
 
-say "Pulling in some common javascripts"
+def setup_database
+  say "Setting up database config", :yellow
+  template "mysql_database.yml.erb", "config/database.yml", :force => true
+  say "WARNING: Answering yes will drop databases if they already exist!", :red
+  if yes?("Create databases '#{app_name}_dev' and '#{app_name}_test'?")
+    rake "db:drop db:create"
+  end
+end
 
-trout "public/javascripts/prefilled_input.js"
+def setup_german_locale
+  say "Setting up german locale", :yellow
+  trout "config/locales/de.yml"
+  gsub_file 'config/application.rb', '# config.i18n.default_locale = :de', 'config.i18n.default_locale = :de'
+end
 
-say "Documentation"
-
-copy_file "README_FOR_SUSPENDERS", "doc/README_FOR_SUSPENDERS"
-
-say "Get ready for bundler... (this will take a while)"
-
-trout 'Gemfile'
-run "bundle install"
-
-say "Let's use MySQL"
-
-template "mysql_database.yml.erb", "config/database.yml", :force => true
-rake "db:create"
-
-say "Setting up plugins"
-
-generators_config = <<-RUBY
+def setup_viennese_timezone
+  say "Setting up viennese timezone", :yellow
+  gsub_file 'config/application.rb', "# config.time_zone = 'Central Time (US & Canada)'", "config.time_zone = 'Vienna'"
+end
+  
+def update_generators_config
+  say "Updating config for generators", :yellow
+  generators_config = <<-RUBY
     config.generators do |generate|
+      generate.stylesheets false
       generate.test_framework :rspec
     end
-RUBY
-inject_into_class "config/application.rb", "Application", generators_config
+  RUBY
+  inject_into_class "config/application.rb", "Application", generators_config
+end
 
-action_mailer_host "development", "#{app_name}.local"
-action_mailer_host "test",        "example.com"
-action_mailer_host "staging",     "staging.#{app_name}.com"
-action_mailer_host "production",  "#{app_name}.com"
+def create_application_layout_and_views
+  say "Creating application layout and shared views", :yellow
+  trout "app/views/layouts/application.html.erb"
+  trout "app/views/shared/_flashes.html.erb"
+end
 
-generate "rspec:install"
-generate "cucumber:install", "--rspec --capybara"
-generate "clearance"
-generate "clearance_features"
+def install_misc_support_files
+  say "Installing miscellaneous support files", :yellow
+  trout "config/initializers/errors.rb"
+  trout "app/helpers/body_class_helper.rb"
+end
 
-create_file "public/stylesheets/sass/screen.scss"
-create_file "public/stylesheets/screen.css"
+def install_compass
+  say "Installing compass", :yellow
+  run "compass init rails . --sass-dir app/stylesheets --css-dir public/stylesheets -q"
+  inside 'app/stylesheets' do
+    remove_file 'ie.scss'
+    remove_file 'print.scss'
+    remove_file 'screen.scss'
+    empty_directory 'content'
+    empty_directory 'vendor'
+  end
+  trout "app/stylesheets/application.scss" 
+  trout "app/stylesheets/ie.scss" 
+end
 
-copy_file "factory_girl_steps.rb", "features/step_definitions/factory_girl_steps.rb"
+def install_formtastic
+  say "Installing formtastic", :yellow
+  generate "formtastic:install"
+  remove_file "public/stylesheets/formtastic.css"
+  remove_file "public/stylesheets/formtastic-changes.css"
+  trout "lib/templates/erb/scaffold/_form.html.erb"
+end
 
-replace_in_file "spec/spec_helper.rb", "mock_with :rspec", "mock_with :mocha"
+def install_sprockets_and_jquery
+  say "Installing sprockets, jQuery, and some other javascripts", :yellow
+  plugin 'sprockets-rails', :git => 'git://github.com/gmoeck/sprockets-rails.git'
+  plugin 'jrails', :git => 'git://github.com/die-antwort/jrails.git'
+  route 'SprocketsApplication.routes(self)'
+  remove_dir 'public/javascripts'
+  remove_file "app/javascripts/application.js"
+  trout "app/javascripts/application.js"
+  trout "vendor/sprockets/modernizr/src/modernizr.js" 
+  trout "vendor/sprockets/jquery-ujs/src/rails.js" 
+end
 
-inject_into_file "features/support/env.rb",
-                 %{Capybara.save_and_open_page_path = 'tmp'\n} +
-                 %{Capybara.javascript_driver = :akephalos\n},
-                 :before => %{Capybara.default_selector = :css}
-replace_in_file "features/support/env.rb",
-                %r{require .*capybara_javascript_emulation.*},
-                ''
+def install_rspec_and_cucumber
+  say "Installing rspec and cucumber", :yellow
+  generate "rspec:install"
+  generate "cucumber:install", "--rspec --capybara"
+  inject_into_file "features/support/env.rb",
+                   %{Capybara.save_and_open_page_path = "tmp"\n} +
+                   %{Capybara.javascript_driver = :akephalos\n},
+                   :before => %{Capybara.default_selector = :css}
+#  replace_in_file "features/support/env.rb",
+#                  %r{require .*capybara_javascript_emulation.*},
+#                  ''
+  copy_file "factory_girl_steps.rb", "features/step_definitions/factory_girl_steps.rb"
+end
 
-rake "flutie:install"
+def cleanup
+  say "Cleaning up", :yellow
+  remove_file 'README'
+  remove_file 'public/index.html'
+  remove_file 'public/images/rails.png'
+end
 
-say "Ignore the right files"
+create_gemfile_and_install_gems
+add_staging_environment
+setup_database
+setup_german_locale
+setup_viennese_timezone
+update_generators_config
+create_application_layout_and_views
+install_misc_support_files
+install_compass
+install_formtastic
+install_sprockets_and_jquery
+install_rspec_and_cucumber
+cleanup
 
-concat_file "suspenders_gitignore", ".gitignore"
-empty_directory_with_gitkeep "app/models"
-empty_directory_with_gitkeep "app/views/pages"
-empty_directory_with_gitkeep "db/migrate"
-empty_directory_with_gitkeep "log"
-empty_directory_with_gitkeep "public/images"
-empty_directory_with_gitkeep "spec/support"
-
-say "Copying miscellaneous support files"
-
-copy_file "errors.rb", "config/initializers/errors.rb"
-copy_file "time_formats.rb", "config/initializers/time_formats.rb"
-copy_file "body_class_helper.rb", "app/helpers/body_class_helper.rb"
-
-say "Setting up a root route"
-
-route "root :to => 'Clearance::Sessions#new'"
-
-say "Congratulations! You just pulled our suspenders."
-say "Remember to run 'rails generate hoptoad' with your API key."
-
+say "Rails app #{app_name} has been created successully!", :blue
+say "Remember to run 'rails generate hoptoad' with your API key.", :blue
